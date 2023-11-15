b8d4f5a6066439baea8973aed70f17bcb383b4f3
Tabelskii Kirill <k.tabelskiy@lightmap.com>
2023-11-15 17:34:21 +0200

    cmfs

---
+++ cmds.sh
@@ -0,0 +1,3 @@
+odoo  shell --db_host ${DB_HOST} -d ${DB_NAME} --db_user ${DB_USER} -w ${DB_PASSWORD}  --no-http
+odoo --db_host ${DB_HOST} -d ${DB_NAME} --db_user ${DB_USER} -w ${DB_PASSWORD}  --no-http --stop-after-init -u order
+odoo --db_host ${DB_HOST} -d ${DB_NAME} --db_user ${DB_USER} -w ${DB_PASSWORD}  --no-http --stop-after-init -u aram_agent_status
