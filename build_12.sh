158d17833d020f9823a784743830017d6cf45971
KirillTabelskiiArammeem <k.tabelskii@arammeem.com>
2024-01-11 11:59:31 +0200

    init

--- build_12.sh
+++ build_12.sh
@@ -5,7 +5,7 @@ docker image rm odoo-modules-crm-12-odoo_test --force;
 docker image rm odoo-modules-crm-12-odoo --force;
 docker image rm odoo-modules-crm-12-python_interpreter --force;
 docker image rm arammeem16/odoo_base  --force;
-docker image rm arammeem16/odoo_core --force;
+docker image rm arammeem16/odoo_core:12.0-latest --force;
 
 cd ~/aram/devops/docker-image-sources
 git pull --rebase
