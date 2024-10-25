0f3aa990e0a551854fad2160925d035217f51185
KirillTabelskiiArammeem <k.tabelskii@arammeem.com>
2024-10-25 13:51:42 +0300

    init

---
+++ kratos.sh
@@ -0,0 +1,10 @@
+kratos new helpdesk-facade -r git@gitlab.zeus.arammeem.net:platform/go/kratos-layout.git
+
+kratos proto add api/application/helpdesk.proto
+
+kratos proto client api/application/helpdesk.proto
+
+kratos proto client api/application/error_reason.proto
+
+kratos proto server api/application/helpdesk.proto -t internal/service
+
