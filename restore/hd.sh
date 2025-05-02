4840329d760b2d95195289a1dd3c13b25dea9701
KirillTabelskiiArammeem <k.tabelskii@arammeem.com>
2025-05-02 16:52:32 +0300

    init

--- restore/hd.sh
+++ restore/hd.sh
@@ -20,6 +20,7 @@ psql --host 127.0.0.1 -U odoo  -d helpdesk -c "UPDATE res_users SET password = '
 psql --host 127.0.0.1 -U odoo  -d helpdesk -c "UPDATE res_users SET password_write_date = '$(date '+%Y-%m-%d %H:%M:%S')' WHERE login = 'admin'"
 psql --host 127.0.0.1 -U odoo  -d helpdesk -c "UPDATE ir_config_parameter SET value = 'http://127.0.0.1:8069' WHERE key = 'web.base.url'"
 
-sudo cp -r /tmp/db/filestore/*  ~/aram/helpdesk/odoo-modules-hd-12/datadir/filestore/helpdesk
-sudo chmod -R 777 ~/aram/helpdesk/odoo-modules-hd-12/datadir/
+mkdir -p ~/aram/helpdesk/helpdesk/datadir/filestore/helpdesk
+sudo cp -r /tmp/db/filestore/*  ~/aram/helpdesk/helpdesk/datadir/filestore/helpdesk
+sudo chmod -R 777 ~/aram/helpdesk/helpdesk/datadir/
 rm -rf /tmp/db
